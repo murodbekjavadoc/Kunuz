5e0aedf9639d49c52eb7f1b13cdbbaa6a0e5a519
masterPanda <murodbekalishovich94@gmai.com>
2024-01-23 11:58:32 +0500

    Auth ready

---
+++ src/main/java/main/SpringAppApplication.java
@@ -0,0 +1,14 @@
+package main;
+
+import main.servise.InitService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
